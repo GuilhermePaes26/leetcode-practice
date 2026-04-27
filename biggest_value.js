bf90347726922ee50419867477c7ef3b62d30a98
Guilherme Paes <paesguigo@gmail.com>
2026-04-27 12:52:59 -0300

    feat: add biggest_value.js

---
+++ biggest_value.js
@@ -0,0 +1,13 @@
+// soma utilizando função recursiva
+
+let numbers = [2, 4, 6, 127, 24, 32, 128];
+
+function getmaiornumber(array) {
+  if (array.length === 1) return array[0];
+
+  let maiorDoResto = getmaiornumber(array.slice(1));
+
+  return array[0] > maiorDoResto ? array[0] : maiorDoResto;
+}
+
+console.log(getmaiornumber(numbers)); // Resultado: 32
